732338bc30687e5f94a198dcd2062866352e7983
saewoo <wooozooo1123@gmail.com>
2022-02-14 21:31:44 +0900

    Fibonacci

--- 예제/Fibonacci/Fibonacci.c
+++ 예제/Fibonacci/Fibonacci.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int a1 = 1;
-    int a2 = 1;
+    int a2 = 2;
     int next = 0;
-    int totalsum = 0;
+    int totalsum = 2;
 
-    while (a2 < 1000000) {
-        
+    while (1)
+    {
         next = a1 + a2;
+        if (next <= 1000000 && next % 2 == 0)
+        {
+            totalsum += next;
+        }
+        else if (next > 1000000)
+        {
+            break;
+        }
+
         a1 = a2;
         a2 = next;
     }
+    printf("%d", totalsum);
 
+    return 0;
 }
